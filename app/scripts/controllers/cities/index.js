a94816f84ee59762a8296301f80f759681eafb4c
Ash Blue <ash@blueashes.com>
2015-11-04 00:05:43 -0700

    Removed lightbox due to some bugs and fixed a sorting bug

--- app/scripts/controllers/cities/index.js
+++ app/scripts/controllers/cities/index.js
@@ -10,6 +10,11 @@
 angular.module('interviewApp')
   .controller('CitiesIndexCtrl', function ($scope, City) {
     var cities = City.query(function () {
+      // No name attribute, so why don't we add one by adjusting the description
+      for (var i = 0; i < cities.length; i++) {
+        cities[i].name = cities[i].description.replace(cities[i].id, '').trim();
+      }
+
       $scope.cities = cities;
     });
   });
