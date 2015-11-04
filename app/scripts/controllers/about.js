38a245532ba259387ca6bc2ccc1a9cb0d3ffe3e9
Ash Blue <ash@blueashes.com>
2015-11-03 20:26:27 -0700

    Added initial Yo Angular scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name interviewApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the interviewApp
+ */
+angular.module('interviewApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name interviewApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the interviewApp
+ */
+angular.module('interviewApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
