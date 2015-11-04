bd251adfd608832c7ed5cf5c1fbfaf1cb3a39422
Ash Blue <ash@blueashes.com>
2015-11-04 10:59:57 -0700

    Photos are now a directive

---
+++ app/scripts/directives/photo.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name interviewApp.directive:photo
+ * @description
+ * # photo
+ */
+angular.module('interviewApp')
+  .directive('photo', function () {
+    return {
+      templateUrl: 'directives/photo.html',
+      restrict: 'E',
+      replace: true
+    };
+  });
